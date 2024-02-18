c225f39c5c07053942855c42071c7e02ee5f578b
seonggon do <mooh2jj@naver.com>
2024-02-18 12:12:48 +0900

    feat: todo add 페이지 진행

--- src/router/todoRouter.js
+++ src/router/todoRouter.js
@@ -5,6 +5,7 @@ import { Navigate } from "react-router-dom";
 const Loading = <div>Loading....</div>;
 const TodoList = lazy(() => import("../pages/todo/ListPage"));
 const TodoRead = lazy(() => import("../pages/todo/ReadPage"));
+const TodoAdd = lazy(() => import("../pages/todo/AddPage"));
 
 const todoRouter = () => {
   return [
@@ -25,6 +26,14 @@ const todoRouter = () => {
       element: (
         <Suspense fallback={Loading}>
           <TodoRead />
+        </Suspense>
+      ),
+    },
+    {
+      path: "add",
+      element: (
+        <Suspense fallback={Loading}>
+          <TodoAdd />
         </Suspense>
       ),
     },
