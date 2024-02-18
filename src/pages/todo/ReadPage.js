d521fb9769ae4a24b0179e1996e2e6bfc41e54d1
seonggon do <mooh2jj@naver.com>
2024-02-18 13:10:10 +0900

    update: 동적페이지 이동

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,11 +1,41 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import {
+  useSearchParams,
+  useNavigate,
+  useParams,
+  createSearchParams,
+} from "react-router-dom";
 
 const ReadPage = (props) => {
+  const navigate = useNavigate();
   const { tno } = useParams();
 
+  const [queryParams] = useSearchParams();
+
+  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
+  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
+
+  const queryStr = createSearchParams({ page: page, size: size }).toString();
+
   console.log(tno);
-  return <div className={"text-3xl"}>Todo Read Page</div>;
+
+  const moveToModify = (tno) => {
+    navigate({ pathname: `/todo/modify/${tno}`, search: queryStr });
+  };
+
+  const moveToList = () => {
+    navigate({ pathname: "/todo/list", search: queryStr });
+  };
+
+  return (
+    <div className={"text-3xl"}>
+      Todo Read Page {tno}
+      <div>
+        <button onClick={() => moveToModify}>Test Modify </button>
+        <button onClick={moveToList}>Test List </button>
+      </div>
+    </div>
+  );
 };
 
 export default ReadPage;
