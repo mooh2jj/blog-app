cd95d0e9a162cc241bcf667d2f0850643d95435a
seonggon do <mooh2jj@naver.com>
2024-03-16 18:35:56 +0900

    update: 상품 리스트 컴포넌트 추가

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -2,11 +2,21 @@ import axios from "axios";
 
 export const API_SERVER_HOST = "http://localhost:8090";
 
-const prefix = `${API_SERVER_HOST}/api/products`;
+const host = `${API_SERVER_HOST}/api/products`;
 
 export const addProduct = async (product) => {
   const haeder = { headers: { "Content-Type": "multipart/form-data" } };
 
-  const res = await axios.post(`${prefix}`, product, haeder);
+  const res = await axios.post(`${host}`, product, haeder);
+  return res.data;
+};
+
+export const getList = async (pageParam) => {
+  const { page, size } = pageParam;
+
+  const res = await axios.get(`${host}/list`, {
+    params: { page: page, size: size },
+  });
+
   return res.data;
 };
--- src/pages/products/ListPage.js
+++ src/pages/products/ListPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function ListPage(props) {
+const ListPage = () => {
   return (
     <div className="p-4 w-full bg-white">
       <div className="text-3xl font-extrabold">Products List Page</div>
+
+      {/* <ListComponent /> */}
     </div>
   );
-}
+};
 
 export default ListPage;
